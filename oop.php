9c566b58c31eded864c480541ca7b58d80a498cd
thesagor <nilsagor004@gmail.com>
2024-12-29 11:06:16 +0600

    updated

--- oop.php
+++ oop.php
@@ -1,11 +1,17 @@
 <?php
 class varmy{
+    public $school= "gvt scool";
+    public $color;
+    public $brand;
     public $name="karim";
-    public function biva($name="biva"){
-        return "$name is my nepheew";
+    public function car($brand, $color ){
+    return "$brand is the best color of car and the $color is top notch";
 
     }
 }
 
+
 $myvar= new varmy;
-var_dump($myvar);
+$myvar->color="red";
+echo $myvar->color;
+
