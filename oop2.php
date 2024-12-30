06c6886a26ff2f583f808f7120c7313476ec793a
Sagor Ahmed <nilsagor004@gmail.com>
2024-12-30 10:29:40 +0600

    2nd class has beeen added

--- oop2.php
+++ oop2.php
@@ -1,1 +1,24 @@
 <?php
+
+class validator
+{
+    private $error = [];
+
+    public function validate($data, $rules)
+    {
+        $this->error = "email has been required";
+    }
+
+    public function fails()
+    {
+        return !empty($this->error);
+    }
+}
+
+$validate = new validator;
+$validate->validate(['mail@temai.com'], ['Required']);
+if ($validate->fails()) {
+    die("has DIe");
+}
+
+
